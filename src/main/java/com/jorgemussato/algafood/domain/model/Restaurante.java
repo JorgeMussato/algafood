538ae954bf8db4759096d8681f66d0cef3d962a4
Jorge Mussato <jorge.mussato@gmail.com>
2019-12-22 14:20:41 -0300

    Adicionado endereço embedded na classe restaurante

--- src/main/java/com/jorgemussato/algafood/domain/model/Restaurante.java
+++ src/main/java/com/jorgemussato/algafood/domain/model/Restaurante.java
@@ -32,6 +32,10 @@ public class Restaurante {
     private Cozinha cozinha;
 
     @JsonIgnore
+    @Embedded
+    private Endereco endereco;
+
+    @JsonIgnore
     @ManyToMany
     @JoinTable(name = "tb_restaurante_forma_pagamento",
             joinColumns = @JoinColumn(name = "restaurante_id"),
